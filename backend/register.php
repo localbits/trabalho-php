f964b8d8d59bd97dc92693cd945f13a63fbe2459
GitMicher <eric.s.nakano@gmail.com>
2026-06-19 00:46:26 -0300

    Useless code 🥀

--- backend/register.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    require 'functions.php';
-
-    if(isset($_POST['insert_user'])) {
-        insert_user($server_connection, $_POST['username'], $_POST['login'], $_POST['email'], $_POST['password']);
-    }
-?>
